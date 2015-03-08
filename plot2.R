146b8f85d0d20178160b253570a50004e5b826a5
smazraq <smazraq@yahoo.com>
2015-03-07 21:46:43 -0500

    Explicit sizing of the PNG files
    added “width = 480, height = 480” to the png calls

--- plot2.R
+++ plot2.R
@@ -1,10 +1,10 @@
 # Plot2.R
 
 # Grab the header
-dataheader <- read.table("./data/household_power_consumption.txt",sep=";", header = TRUE,nrow = 1)
+dataheader <- read.table("../Data/household_power_consumption.txt",sep=";", header = TRUE,nrow = 1)
 
 # Pull only the 2/1/2007 and 2/2/2007 data
-data <- read.table("./data/household_power_consumption.txt",sep=";", skip = 66637, header = FALSE,nrow=2880)
+data <- read.table("../Data/household_power_consumption.txt",sep=";", skip = 66637, header = FALSE,nrow=2880)
 
 # Put the header on the data
 colnames(data) <- colnames(dataheader)
@@ -13,7 +13,7 @@ colnames(data) <- colnames(dataheader)
 data$datetime <- strptime(paste(data$Date,data$Time),format = "%d/%m/%Y %T")
 
 # Open device to plot to
-png(filename = "./plot2.png")
+png(filename = "./plot2.png",width = 480, height = 480)
 
 #Plot 2
 with(data,plot(datetime,Global_active_power,type="n",xlab = "",ylab = "Global Active Power (kilowatts)"))
--- plot4.R
+++ plot4.R
@@ -1,10 +1,10 @@
 # Plot1.R
 
 # Grab the header
-dataheader <- read.table("./data/household_power_consumption.txt",sep=";", header = TRUE,nrow = 1)
+dataheader <- read.table("../Data/household_power_consumption.txt",sep=";", header = TRUE,nrow = 1)
 
 # Pull only the 2/1/2007 and 2/2/2007 data
-data <- read.table("./data/household_power_consumption.txt",sep=";", skip = 66637, header = FALSE,nrow=2880)
+data <- read.table("../Data/household_power_consumption.txt",sep=";", skip = 66637, header = FALSE,nrow=2880)
 
 # Put the header on the data
 colnames(data) <- colnames(dataheader)
@@ -13,7 +13,7 @@ colnames(data) <- colnames(dataheader)
 data$datetime <- strptime(paste(data$Date,data$Time),format = "%d/%m/%Y %T")
 
 # Open device to plot to
-png(filename = "./plot4.png")
+png(filename = "./plot4.png",width = 480, height = 480)
 
 #plot 4
 par(mfrow = c(2, 2))
